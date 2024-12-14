1b656132c7f206c7906f911e7f22f57723fe169a
Hidetaka <u687502j@ecs.osaka-u.ac.jp>
2024-12-15 01:43:03 +0900

    no debug

--- app.py
+++ app.py
@@ -160,5 +160,5 @@ def match_history():
 
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    #app.run(debug=True)
